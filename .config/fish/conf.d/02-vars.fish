08fef96f62ade39e2ad0f605c613858fa245418c
Kyle Colantonio <k@yle.sh>
2026-02-07 16:50:32 -0500

    fix: HW acceleration in browsers

--- .config/fish/conf.d/02-vars.fish
+++ .config/fish/conf.d/02-vars.fish
@@ -1,3 +1,6 @@
 set -gx B2_APPLICATION_KEY_ID $(cat "$HOME/.config/b2/api_key" | sed '1!d')
 set -gx B2_APPLICATION_KEY $(cat "$HOME/.config/b2/api_key" | sed '2!d')
 set -gx CLOUDFLARE_API_TOKEN $(cat "$HOME/.config/cloudflare/api_token")
+
+# Firefox hardware acceleration fix for NVIDIA
+set -gx MOZ_DISABLE_RDD_SANDBOX 1
