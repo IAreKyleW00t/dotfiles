3e8d2029e9f183ec0bc7ac8d8bbc7b13eb11eee9
Kyle Colantonio <k@yle.sh>
2026-02-08 00:53:31 -0500

    chore: Updating comment

--- .config/fish/conf.d/02-vars.fish
+++ .config/fish/conf.d/02-vars.fish
@@ -1,3 +1,4 @@
+# API tokens
 set -gx B2_APPLICATION_KEY_ID $(cat "$HOME/.config/b2/api_key" | sed '1!d')
 set -gx B2_APPLICATION_KEY $(cat "$HOME/.config/b2/api_key" | sed '2!d')
 set -gx CLOUDFLARE_API_TOKEN $(cat "$HOME/.config/cloudflare/api_token")
