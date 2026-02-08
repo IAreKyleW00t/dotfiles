f689bf94421f101ad9fe2a597c1e916778ec959e
Kyle Colantonio <k@yle.sh>
2026-02-08 11:47:53 -0500

    feat: Using yay-bin

--- .config/fish/functions/yay-update.fish
+++ .config/fish/functions/yay-update.fish
@@ -1,9 +1,10 @@
 # Utility function to update yay
 function yay-update
-    set -q YAY_SRC_DIR || set YAY_SRC_DIR "$HOME/.local/src/yay"
+    set -q YAY_REPO || set YAY_REPO "yay-bin" # or yay
+    set -q YAY_SRC_DIR || set YAY_SRC_DIR "$HOME/.local/src/$YAY_REPO"
     mkdir -p "$YAY_SRC_DIR"
     if not test -d "$YAY_SRC_DIR/.git"
-        git clone https://aur.archlinux.org/yay.git "$YAY_SRC_DIR"
+        git clone "https://aur.archlinux.org/$YAY_REPO.git" "$YAY_SRC_DIR"
     end
     fish -c "cd '$YAY_SRC_DIR' && git pull && makepkg -si"
 end
