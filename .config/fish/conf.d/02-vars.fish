ef3e90c690ae8726c19b4916dc98201046bd2abc
Kyle Colantonio <k@yle.sh>
2026-02-08 01:11:11 -0500

    feat: Updating global environment variables

--- .config/fish/conf.d/02-vars.fish
+++ .config/fish/conf.d/02-vars.fish
@@ -2,12 +2,3 @@
 set -gx B2_APPLICATION_KEY_ID $(cat "$HOME/.config/b2/api_key" | sed '1!d')
 set -gx B2_APPLICATION_KEY $(cat "$HOME/.config/b2/api_key" | sed '2!d')
 set -gx CLOUDFLARE_API_TOKEN $(cat "$HOME/.config/cloudflare/api_token")
-
-# Firefox hardware acceleration fix for NVIDIA
-# NOTE: must also be set on .desktop files
-set -gx MOZ_DISABLE_RDD_SANDBOX 1
-set -gx LIBVA_DRIVER_NAME nvidia
-set -gx CUDA_DISABLE_PERF_BOOST 1
-
-# Podman
-set -gx DOCKER_HOST "unix://$XDG_RUNTIME_DIR/podman/podman.sock"
