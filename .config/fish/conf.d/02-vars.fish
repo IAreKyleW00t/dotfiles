d334139f47436683a97df284d107c067fa562693
Kyle Colantonio <k@yle.sh>
2026-02-07 21:13:17 -0500

    feat: HW accel and podman

--- .config/fish/conf.d/02-vars.fish
+++ .config/fish/conf.d/02-vars.fish
@@ -3,4 +3,10 @@ set -gx B2_APPLICATION_KEY $(cat "$HOME/.config/b2/api_key" | sed '2!d')
 set -gx CLOUDFLARE_API_TOKEN $(cat "$HOME/.config/cloudflare/api_token")
 
 # Firefox hardware acceleration fix for NVIDIA
+# NOTE: must also be set on .desktop files
 set -gx MOZ_DISABLE_RDD_SANDBOX 1
+set -gx LIBVA_DRIVER_NAME nvidia
+set -gx CUDA_DISABLE_PERF_BOOST 1
+
+# Podman
+set -gx DOCKER_HOST "unix://$XDG_RUNTIME_DIR/podman/podman.sock"
